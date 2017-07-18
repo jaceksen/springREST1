c4d7aef71d651270cd49be75cdd378c5162b0aa1
Jacek Seń <jaceksen@gmail.com>
2017-07-18 10:25:57 +0200

    170718_1

--- src/main/java/pl/com/erkin/springbootstarter/topic/TopicController.java
+++ src/main/java/pl/com/erkin/springbootstarter/topic/TopicController.java
@@ -3,21 +3,28 @@ package pl.com.erkin.springbootstarter.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	
+	
+	//I ma declaring a dependency to TopicService
+	@Autowired
+	private TopicService topicService;
+	
 
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopisc() {
-		return Arrays.asList(
-				
-				new Topic("spirng","Spring Framework","Spring Framework Description"),
-				new Topic("java","Core Java","Core Java Description"),
-				new Topic("javascript","JavaScript","JavaScript Description")
-				
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 	
 }
