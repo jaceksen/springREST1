b04751c71587b2fbb7e9f7d9ee8c6731a8f8da35
Jacek Seń <jaceksen@gmail.com>
2017-07-21 18:36:43 +0200

    170721_1

--- src/main/java/pl/com/erkin/springbootstarter/topic/TopicController.java
+++ src/main/java/pl/com/erkin/springbootstarter/topic/TopicController.java
@@ -5,26 +5,46 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
-	
-	//I ma declaring a dependency to TopicService
+
+	// I ma declaring a dependency to TopicService
 	@Autowired
 	private TopicService topicService;
-	
 
+	// GET implementation
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopisc() {
 		return topicService.getAllTopics();
 	}
-	
+
+	// GET implementation
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
-	
+
+	// POST implementation
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+
+	// PUT implementation
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+
+	// DELETE implementation
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+
 }
